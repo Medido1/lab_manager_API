bebd003b1479b5bfbde1adc378507917901d4adc
medido <medi1222@gmail.com>
2026-01-20 20:13:02 +0100

    Add rate limiting for login

--- src/Routes/usersRouter.js
+++ src/Routes/usersRouter.js
@@ -1,11 +1,18 @@
 import {Router} from "express";
 import { registerUser, userLogin, getCurrentUser } from "../controllers/userController.js";
 import { authenticateJWT } from "../middleware/auth.js";
+import rateLimit from "express-rate-limit";
 
 const usersRouter = Router();
 
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 5,
+  message: "Too many login attempts. Try again later."
+});
+
 usersRouter.post('/register',registerUser);
-usersRouter.post('/login', userLogin);
+usersRouter.post('/login', loginLimiter, userLogin);
 usersRouter.get('/profile', authenticateJWT, getCurrentUser);
 
 export default usersRouter;
